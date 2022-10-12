30aed8d44d28f9769d623365805177577c3af8cb
GToidZ <39559733+GToidZ@users.noreply.github.com>
2022-10-12 10:41:48 +0700

    Removed requirements.txt and auto-formatted source

--- test_statistics.py
+++ test_statistics.py
@@ -3,12 +3,11 @@ from statistics import variance
 
 
 class StatisticsTest(TestCase):
-
     def test_variance_typical_values(self):
         """variance of typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
+        self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
 
     def test_variance_throws_exception(self):
         """variance of an empty list should raise an exception"""
@@ -16,7 +15,7 @@ class StatisticsTest(TestCase):
             var = variance([])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import unittest
-    unittest.main(verbosity=1)
 
+    unittest.main(verbosity=1)
